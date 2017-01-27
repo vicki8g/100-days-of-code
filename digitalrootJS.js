b5055310c856b4f2f7d572cd5c69749059a312ad
Vicki <vicki8g@yahoo.com>
2017-01-26 21:46:29 -0600

    Update digitalrootJS.js

--- digitalrootJS.js
+++ digitalrootJS.js
@@ -17,6 +17,13 @@ function digital_root(n) {
 
 /* Built-in objects/methods used:
  parseInt() function- parses a string argument and returns an integer of the specified radix (the base in mathematical numeral systems).
+              parseInt("20px");       // 20
+              parseInt("10100", 2);   // 20 binary
+              parseInt("2e1");        // 2 exponent
+
+              // type conversion
+              Number("20px");       // NaN
+              Number("2e1");        // 20, exponential notation 10^# indicated after the e
  toString() method converts a number to a string
  split() method splits a String object into an array of strings by separating the string into substrings.
  forEach() method executes a provided function once for each array element.
