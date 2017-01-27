73c51a0547a9148aa61610a0a0173ee2b18833ca
Vicki <vicki8g@yahoo.com>
2017-01-27 14:20:39 -0600

    how to add elements to array in a for loop
    the array must be a global variable so that it isn't replaced locally in the for or if statement

---
+++ loopArray.js
@@ -0,0 +1,11 @@
+function friend(friends){
+
+ var array = [];
+  for(var i = 0; i < friends.length; i++){
+      if(4 === friends[i].length){
+        array.push(friends[i]);
+      }
+     }
+   return array;
+  //
+}
